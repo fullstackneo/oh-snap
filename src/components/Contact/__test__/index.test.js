47afdece7b786303863c1d2f794fb2445ddd08c2
Neo <fullstackneo@gmail.com>
2022-06-02 18:25:15 -0600

    update test

---
+++ src/components/Contact/__test__/index.test.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '..';
+
+afterEach(cleanup);
+
+describe('Nav component', () => {
+  it('renders', () => {
+    render(<Contact />);
+  });
+
+  it('matches snapshot DOM node structure', () => {
+    const { asFragment } = render(<Contact />);
+    expect(asFragment()).toMatchSnapshot();
+  });
+});

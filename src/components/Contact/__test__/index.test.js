373878e425b41cdecdf06d81ecef6e5404ae8534
Neo <fullstackneo@gmail.com>
2022-06-03 14:02:01 -0600

    add test for Modal component

--- src/components/Contact/__test__/index.test.js
+++ src/components/Contact/__test__/index.test.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Contact from '..';
